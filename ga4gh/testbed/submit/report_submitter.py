f5127695e333f59735afb156163a1906dc92a3dc
Chen Chen <chenchen199727@gmail.com>
2024-09-06 10:15:10 -0400

    updated private submit in request body

--- ga4gh/testbed/submit/report_submitter.py
+++ ga4gh/testbed/submit/report_submitter.py
@@ -27,8 +27,11 @@ class ReportSubmitter():
             results["error_message"] = "Report submitted is not a GA4GH Report object"
             return results
 
+        json_report = json.loads(report.to_json())
+        json_report['private'] = private_submit
+
         header = {"GA4GH-TestbedReportSeriesId": series_id, "GA4GH-TestbedReportSeriesToken": series_token, "private": private_submit}
-        submit_request = requests.post(url, headers=header ,json=json.loads(report.to_json()))        
+        submit_request = requests.post(url, headers=header ,json=json_report)        
 
         results["status_code"] = submit_request.status_code
 
